8d53cbb63cc8fb5941692af22fe907927a44692a
Colin Campbell <colinc86@gmail.com>
2020-01-20 19:07:22 -0600

    Update package file.

--- Package.swift
+++ Package.swift
@@ -3,5 +3,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "NeuralNet"
+    name: "NeuralNet",
+    products: [
+        .library(
+            name: "NeuralNet",
+            targets: ["NeuralNet"])
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "NeuralNet",
+            dependencies: [])
+    ]
 )
